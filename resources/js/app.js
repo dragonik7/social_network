eb573897552dac6da90b72f489d18a153e013cd1
shami <Shamil79797@gmail.com>
2023-02-13 08:37:48 +0300

    add frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import {createApp} from "vue";
+import App from "./App.vue";
+import router from "./router/router.js";
+import store from "./store";
+
+console.log()
+
+createApp(App)
+	.use(store)
+	.use(router)
+	.mount("#app");
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="icon" href="{{asset('assets/logo.png')}}">
+	<title>Laravel</title>
+</head>
+@vite('resources/js/app.js')
+
+<body>
+	<div id="app">
+
+	</div>
+</body>
+</html>
