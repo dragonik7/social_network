29920685554d2dd091f2f9ad07c78b3dc188b0a0
shami <Shamil79797@gmail.com>
2023-02-15 17:13:32 +0300

    che

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,14 @@ import router from "./router/router.js";
 import store from "./store";
 
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {faYoutube} from '@fortawesome/free-brands-svg-icons'
+
+library.add(faYoutube)
+
 createApp(App)
+	.component('font-awesome-icon', FontAwesomeIcon)
 	.use(store)
 	.use(router)
 	.mount("#app");
