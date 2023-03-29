e439c5d949677a3e424540f520273bcd77d77482
shami <Shamil79797@gmail.com>
2023-03-29 10:42:38 +0300

    delete: all frontend

--- resources/js/Utils/axios.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.baseURL = import.meta.env.APP_URL ?? 'http://localhost'
-axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('Bearer ')
-
